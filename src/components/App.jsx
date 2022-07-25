c274d3000ef02e3647b778ee887fabdbe962e159
Karanin Serhii <36915523+KaraninS@users.noreply.github.com>
2022-07-25 16:32:42 +0300

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,19 @@ import EmptyMessage from './EmptyMessage/EmptyMessage';
 import { nanoid } from 'nanoid';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+  componentDidMount() {
+    const parsedContact = JSON.parse(localStorage.getItem('contacts')) ?? [];
+    this.setState({ contacts: parsedContact });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
   addContact = ({ name, number }) => {
     this.setState(prevState => {
       const { contacts } = this.state;
