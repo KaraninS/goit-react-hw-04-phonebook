1b42ff1bc4e15b1a2f39b2db437e50b917fec259
Karanin Serhii <36915523+KaraninS@users.noreply.github.com>
2022-08-19 20:23:53 +0300

    HW4.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import EmptyMessage from './EmptyMessage/EmptyMessage';
 import { nanoid } from 'nanoid';
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-  componentDidMount() {
-    const parsedContact = JSON.parse(localStorage.getItem('contacts')) ?? [];
-    this.setState({ contacts: parsedContact });
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (prevState !== contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-  addContact = ({ name, number }) => {
-    this.setState(prevState => {
-      const { contacts } = this.state;
-      const allContacts = contacts.reduce((acc, contact) => {
+
+export function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    setContacts(prevState => {
+      const allContacts = prevState.reduce((acc, contact) => {
         acc.push(contact.name.toLocaleLowerCase());
         return acc;
       }, []);
@@ -31,46 +26,43 @@ export class App extends Component {
         return alert(`${name} already in contacts.`);
       }
       const newContact = { id: nanoid(), name, number };
-      return {
-        contacts: [...prevState.contacts, newContact],
-      };
+      return [...prevState, newContact];
     });
   };
-  makeFilteredMarkup = () => {
-    const lowerCaseFilter = this.state.filter.toLocaleLowerCase();
-    const filteredArray = [...this.state.contacts].filter(contact =>
+  const makeFilteredMarkup = () => {
+    const lowerCaseFilter = filter.toLocaleLowerCase();
+    const filteredArray = [...contacts].filter(contact =>
       contact.name.toLocaleLowerCase().includes(lowerCaseFilter)
     );
     return filteredArray;
   };
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-  changeFilter = event => {
-    this.setState({
-      filter: event.target.value,
-    });
-  };
-  render() {
-    return (
-      <div>
-        <h1 className="title_phonebook">Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-        <h2 className="title_contacts">Contacts</h2>
-        {this.state.contacts.length > 0 ? (
-          <>
-            <Filter value={this.state.filter} onChange={this.changeFilter} />
-            <ContactList
-              contacts={this.makeFilteredMarkup()}
-              onDelClick={this.deleteContact}
-            />
-          </>
-        ) : (
-          <EmptyMessage />
-        )}
-      </div>
+
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
     );
-  }
+  };
+
+  const changeFilter = event => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1 className="title_phonebook">Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2 className="title_contacts">Contacts</h2>
+      {contacts.length > 0 ? (
+        <>
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={makeFilteredMarkup()}
+            onDelClick={deleteContact}
+          />
+        </>
+      ) : (
+        <EmptyMessage />
+      )}
+    </div>
+  );
 }
